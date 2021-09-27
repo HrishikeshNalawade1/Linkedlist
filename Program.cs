17091a5385da85a725ab87c3aa2d6c82854286ba
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-27 23:05:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace linklistt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> mylist = new LinkedList<int>();
+            mylist.AddFirst(23);
+            mylist.AddFirst(34);
+            mylist.AddLast(134);
+            mylist.AddLast(354);
+            foreach (int tt in mylist)
+            {
+                Console.Write(tt+" ");
+            }
+            mylist.RemoveLast();
+            Console.WriteLine("\nAfter removing last element");
+            foreach (int tt in mylist)
+            {
+                Console.Write(tt + " ");
+            }
+        }
+       
+    }
+}
